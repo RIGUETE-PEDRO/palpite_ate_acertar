0797f79840a5b881fcbdd71da990beee2af22e09
PEDRO HENRIQUE RIGUETE DE OLIVEIRA MARTINS <154231408+RIGUETE-PEDRO@users.noreply.github.com>
2024-12-31 16:22:09 -0300

    Create sorteio.java

---
+++ sorteio.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+public class sorteio {
+  
+    public static void main(String[] args)
+    {
+        sorteando(new Scanner(System.in));
+    }
+    
+        public static void sorteando(Scanner scanner)
+        {
+            int verificação = 1;
+            int tentativas = 0;
+
+            Random random = new Random();
+            int sorteado = random.nextInt(20000) + 0;
+            System.out.println("estamos comecando o palpite e acerto;\n vamos la qual e seu palpite");
+            
+            while (verificação == 1) 
+            {
+              int palpite = Integer.parseInt(scanner.next());
+
+                if (palpite > sorteado) 
+                {
+                    System.out.println("vc errou o numero e menor\n tente denovo");
+                }
+                if (palpite == sorteado) 
+                {
+                    System.err.println("parabens vc acertou o numero");
+                    verificação ++;
+                    System.out.println("vc acertou com :" +(tentativas+1)+"tentativas");
+                    return;
+                }
+                if (palpite < sorteado) 
+                {   
+                    System.out.println("vc errou o numero e maior\n tente denovo");
+                }
+                tentativas++;
+            }
+        }
+}
